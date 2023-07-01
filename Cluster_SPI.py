af45885ad9b6faa45a1a2f82fb197fbb5c8365d2
Eduardo Lima <ce-lima@hotmail.com>
2023-07-01 12:02:00 -0300

    Kmeans escrito

--- Cluster_SPI.py
+++ Cluster_SPI.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import geopandas as gpd
 import os
 
@@ -44,6 +45,31 @@ def spatial_plot(df, shp, dict_scatter, title, fig_name):
     
     plt.close()
 
+def n_cluster(df, max_cluster, title, fig_name):
+    inertia_list = []
+    for i in range(1, max_cluster+1):
+        km = KMeans(n_clusters = i, random_state = 0).fit(df)
+        inertia_list.append([i, km.inertia_])
+
+    inertia_list = np.array(inertia_list)
+    print(inertia_list[0,1])
+    diff = np.abs(np.diff(inertia_list[:,1], axis = 0)/inertia_list[0:-1,1])
+    fig, ax = plt.subplots(dpi = 600)
+    ax.bar(x = inertia_list[1::,0], height = diff, zorder = 1, tick_label = range(2, max_cluster+1))
+    # ax.bar(x = inertia_list[:,0], height = inertia_list[:,1], zorder = 1, tick_label = range(1, max_cluster+1))
+    # ax.plot(inertia_list[:,0], inertia_list[:,1])
+    # ax.set_xticks(range(1,max_cluster+1))
+    # ax.plot(inertia_list[1::,0], diff)
+    # ax.set_xticks(range(2,max_cluster+1))
+    ax.set_xlabel("n° clusters")
+    ax.set_ylabel("SSE Variation")
+    ax.set_title(title, loc = "left")
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(1))
+    print(diff)
+    fig.savefig(
+        "Figuras/{}.png".format(fig_name),
+        dpi = 600, bbox_inches = "tight", facecolor = "w")
+    return inertia_list
 #%%
 data_spi = pd.read_csv("Dados/df_spi.csv", sep = ";")
 band1 = pd.read_csv("Dados/Banda1.csv", sep = ";", header = None, index_col = 0).T
@@ -65,6 +91,14 @@ spi_df.columns, b1_df.columns, b2_df.columns, b3_df.columns = (pd.to_datetime(sp
                                                             pd.to_datetime(b1_df.columns),
                                                             pd.to_datetime(b2_df.columns),
                                                             pd.to_datetime(b3_df.columns))
+spi_df = spi_df.loc[:, spi_df.columns.month == 12]
+#%%
+
+#Definindo número de Cluster
+n_cluster(spi_df, 10, "a) SPI-12", "SPI-12")
+n_cluster(b1_df, 10, "b) High Frequency Band (2 to 8 years)", "Banda_1_SSE")
+n_cluster(b2_df, 10, "c) Medium Frequency Band (9 to 40 years)", "Banda_2_SSE")
+n_cluster(b3_df, 10, "d) Low Frequency Band (40+ years)", "Banda_3_SSE")
 
 #%%
 kmeans_spi = KMeans(n_clusters = 7, random_state = 0)
@@ -78,7 +112,7 @@ center_spi.to_csv("Dados/spi_Center.csv",
             sep = ";", header = True, index = True)
 
 #%%
-kmeans_b1 = KMeans(n_clusters = 7, random_state = 0)
+kmeans_b1 = KMeans(n_clusters = 6, random_state = 0)
 kmeans_b1.fit(b1_df)
 center_b1 = pd.DataFrame(kmeans_b1.cluster_centers_).T
 center_b1.index = b1_df.columns
@@ -89,7 +123,7 @@ center_b1.to_csv("Dados/band1_Center.csv",
             sep = ";", header = True, index = True)
 
 #%%
-kmeans_b2 = KMeans(n_clusters = 5, random_state = 0)
+kmeans_b2 = KMeans(n_clusters = 3, random_state = 0)
 kmeans_b2.fit(b2_df)
 center_b2 = pd.DataFrame(kmeans_b2.cluster_centers_).T
 center_b2.index = b2_df.columns
@@ -117,7 +151,8 @@ dict_scatter = [
     {"cluster": 3, "color": "darkorange", "label": "Cluster 4"},
     {"cluster": 4, "color": "black", "label": "Cluster 5"},
     {"cluster": 5, "color": "darkmagenta", "label": "Cluster 6"},
-    {"cluster": 6, "color": "darkcyan", "label": "Cluster 7"}
+    {"cluster": 6, "color": "darkcyan", "label": "Cluster 7"},
+    {"cluster": 7, "color": "brown", "label": "Cluster 8"}
 ]
 
 #%%
@@ -129,21 +164,11 @@ spatial_plot(cluster_b3, shp, dict_scatter, "d) Low Frequency Band (40+ years)",
 #%%
 fig, ax = plt.subplots(dpi = 600)
 for i in dict_scatter:
-    ax.scatter(x = cluster_b2.lon.loc[cluster_b2.cluster == i["cluster"]],
-               y = cluster_b2.lat.loc[cluster_b2.cluster == i["cluster"]],
+    ax.scatter(x = cluster_spi.lon.loc[cluster_spi.cluster == i["cluster"]],
+               y = cluster_spi.lat.loc[cluster_spi.cluster == i["cluster"]],
                c = i["color"], marker = "o", s = 10, label = i["label"])
-ax.legend()
+ax.legend(loc = "lower right", markerfirst = True, handletextpad = 0.1,
+        borderaxespad = 0.3, fontsize = 10,  markerscale = 1.5)
 
-#%%
-#%%
-spatial_plot(cluster_SPI, shp, dict_scatter, "a) SPI12-Dez", "SPI12-Dez")
-spatial_plot(cluster_b1, shp, dict_scatter, "b) Banda 1 (2 até 8 anos)", "Banda_1")
-spatial_plot(cluster_b2, shp, dict_scatter, "c) Banda 2 (9 até 40 anos)", "Banda_2")
-spatial_plot(cluster_b3, shp, dict_scatter, "d) Banda 3 (40+ anos)", "Banda_3")
-
-#%%
-center_plot(center_SPI, dict_scatter, "SPI12-Dez", "SPI12-Dez", [-3.3, 3.3])
-center_plot(center_b1, dict_scatter, "Banda 1", "Banda_1", [-3, 3])
-center_plot(center_b2, dict_scatter, "Banda 2", "Banda_2", [-0.8, 0.8])
-center_plot(center_b3, dict_scatter, "Banda 3", "Banda_3", [-1.2, 0.5])
+#
 # %%
